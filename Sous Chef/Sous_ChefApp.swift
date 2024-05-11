c4057a4e5ee875e70127cae0c271296ef0bb6431
marcogracie <marco.gracie22@gmail.com>
2024-05-11 14:37:28 -0400

    Initial Commit

---
+++ Sous Chef/Sous_ChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sous_ChefApp.swift
+//  Sous Chef
+//
+//  Created by Marco Gracie on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Sous_ChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
